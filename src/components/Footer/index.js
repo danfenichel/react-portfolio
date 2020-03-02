e2c364f9c04bd317603b611a9a8f93908a9e6c1d
D0491 <dan.fenichel@yahoo.com>
2020-03-01 18:56:07 -0600

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="copyright">Copyright &copy; 2020</footer>
+    );
+}
+
+export default Footer;
